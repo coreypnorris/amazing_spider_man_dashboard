c6357e6844c7845889bd0f87aacc8d7e7b0bdf03
Corey Norris <coreypnorris@gmail.com>
2014-09-13 11:45:53 -0700

    tweak some things for heroku

--- jobs/get_comics.rb
+++ jobs/get_comics.rb
@@ -1,13 +1,13 @@
 require 'rest_client'
 require 'digest/md5'
 
-SCHEDULER.every '5s', :first_in => 0 do |job|
+SCHEDULER.every '1h', :first_in => 0 do |job|
   timestamp = Time.now.to_s.gsub(/\s+/, "")
-  hash = Digest::MD5.hexdigest(timestamp + settings.marvel_private_key + settings.marvel_public_key)
+  hash = Digest::MD5.hexdigest(timestamp + settings.marvel_private_key.to_s + settings.marvel_public_key.to_s)
 
   returned_data = RestClient::Request.new(
     :method => :get,
-    :url => 'http://gateway.marvel.com:80/v1/public/comics?ts=' + timestamp + '&apikey=' + settings.marvel_public_key + '&hash=' + hash + '&format=comic&dateDescriptor=thisWeek'
+    :url => 'http://gateway.marvel.com:80/v1/public/comics?ts=' + timestamp + '&apikey=' + settings.marvel_public_key.to_s + '&hash=' + hash + '&format=comic&dateDescriptor=thisWeek'
   ).execute
 
   parsed_data = JSON.parse(returned_data)
