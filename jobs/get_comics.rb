50785beb4f3038277c2dd81bfcd8cc7708935bc3
Corey Norris <coreypnorris@gmail.com>
2014-09-13 11:30:57 -0700

    tweak some things for heroku

--- jobs/get_comics.rb
+++ jobs/get_comics.rb
@@ -3,17 +3,17 @@ require 'digest/md5'
 
 SCHEDULER.every '5s', :first_in => 0 do |job|
   timestamp = Time.now.to_s.gsub(/\s+/, "")
-  hash = Digest::MD5.hexdigest(timestamp + settings.marvel_private_key + settings.marvel_public_key)
+  # hash = Digest::MD5.hexdigest(timestamp + settings.marvel_private_key + settings.marvel_public_key)
 
-  returned_data = RestClient::Request.new(
-    :method => :get,
-    :url => 'http://gateway.marvel.com:80/v1/public/comics?ts=' + timestamp + '&apikey=' + settings.marvel_public_key + '&hash=' + hash + '&format=comic&dateDescriptor=thisWeek'
-  ).execute
+  # returned_data = RestClient::Request.new(
+  #   :method => :get,
+  #   :url => 'http://gateway.marvel.com:80/v1/public/comics?ts=' + timestamp + '&apikey=' + settings.marvel_public_key + '&hash=' + hash + '&format=comic&dateDescriptor=thisWeek'
+  # ).execute
 
-  parsed_data = JSON.parse(returned_data)
-  results = parsed_data['data']['results']
+  # parsed_data = JSON.parse(returned_data)
+  # results = parsed_data['data']['results']
 
-  send_event('issue_big_picture', image: results[0]['images'].first['path'] + '.jpg')
+  # send_event('issue_big_picture', image: results[0]['images'].first['path'] + '.jpg')
   send_event('issue_title', title: timestamp)
-  send_event('issue_description', text: settings.marvel_public_key)
+  # send_event('issue_description', text: settings.marvel_public_key)
 end
