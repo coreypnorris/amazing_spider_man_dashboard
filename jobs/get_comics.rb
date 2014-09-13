f42812aacb3faa764cc384a61cd902acd3ac1d61
Corey Norris <coreypnorris@gmail.com>
2014-09-13 11:38:24 -0700

    tweak some things for heroku

--- jobs/get_comics.rb
+++ jobs/get_comics.rb
@@ -3,7 +3,7 @@ require 'digest/md5'
 
 SCHEDULER.every '5s', :first_in => 0 do |job|
   timestamp = Time.now.to_s.gsub(/\s+/, "")
-  # hash = Digest::MD5.hexdigest(timestamp + settings.marvel_private_key + settings.marvel_public_key)
+  hash = Digest::MD5.hexdigest(timestamp + settings.marvel_private_key + settings.marvel_public_key)
 
   # returned_data = RestClient::Request.new(
   #   :method => :get,
@@ -15,5 +15,5 @@ SCHEDULER.every '5s', :first_in => 0 do |job|
 
   # send_event('issue_big_picture', image: results[0]['images'].first['path'] + '.jpg')
   send_event('issue_title', title: timestamp)
-  send_event('issue_description', text: settings.marvel_public_key)
+  send_event('issue_description', text: hash)
 end
