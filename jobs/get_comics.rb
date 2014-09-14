6bfdccc0d12c8b270d2a2d27d47009e7429ae169
Corey Norris <coreypnorris@gmail.com>
2014-09-14 15:04:06 -0700

    add script for layout

--- jobs/get_comics.rb
+++ jobs/get_comics.rb
@@ -3,17 +3,13 @@ require 'digest/md5'
 
 SCHEDULER.every '1h', :first_in => 0 do |job|
   timestamp = Time.now.to_s.gsub(/\s+/, "")
-  hash = Digest::MD5.hexdigest(timestamp + settings.marvel_private_key.to_s + settings.marvel_public_key.to_s)
+  hash = Digest::MD5.hexdigest(timestamp + settings.marvel_private_key + settings.marvel_public_key)
 
   returned_data = RestClient::Request.new(
     :method => :get,
-    :url => 'http://gateway.marvel.com:80/v1/public/comics?ts=' + timestamp + '&apikey=' + settings.marvel_public_key.to_s + '&hash=' + hash + '&format=comic&dateDescriptor=thisWeek'
+    :url => 'http://gateway.marvel.com:80/v1/public/comics?ts=' + timestamp + '&apikey=' + settings.marvel_public_key + '&hash=' + hash + '&format=comic&dateDescriptor=thisWeek'
   ).execute
 
   parsed_data = JSON.parse(returned_data)
-  results = parsed_data['data']['results']
-
-  send_event('issue_big_picture', image: results[0]['images'].first['path'] + '.jpg')
-  send_event('issue_title', title: timestamp)
-  send_event('issue_description', text: hash)
+  $results = parsed_data['data']['results']
 end
--- jobs/show_comics.rb
+++ jobs/show_comics.rb
@@ -1,13 +1,13 @@
-# require 'rest_client'
-# require_relative 'get_comics.rb'
+require 'rest_client'
+require_relative 'get_comics.rb'
 
-# SCHEDULER.every '7s', :first_in => '10s' do |job|
-#   results = $results.rotate!
-#   send_event('issue_big_picture', image: results[0]['images'].first['path'] + '.jpg')
-#   send_event('issue_title', title: results[0]['title'])
-#   send_event('issue_description', text: results[0]['description'])
-#   send_event('preview_thumbnail_1', image: results[1]['thumbnail'].first.last + '.jpg')
-#   send_event('preview_thumbnail_2', image: results[2]['thumbnail'].first.last + '.jpg')
-#   send_event('preview_thumbnail_3', image: results[3]['thumbnail'].first.last + '.jpg')
-#   send_event('preview_thumbnail_4', image: results[4]['thumbnail'].first.last + '.jpg')
-# end
+SCHEDULER.every '5s', :first_in => '10s' do |job|
+  rotated_results = $results.rotate!
+  send_event('issue_big_picture', image: rotated_results[0]['images'].first['path'] + '.jpg')
+  send_event('issue_title', title: rotated_results[0]['title'])
+  send_event('issue_description', text: rotated_results[0]['description'])
+  send_event('preview_thumbnail_1', image: rotated_results[1]['thumbnail'].first.last + '.jpg')
+  send_event('preview_thumbnail_2', image: rotated_results[2]['thumbnail'].first.last + '.jpg')
+  send_event('preview_thumbnail_3', image: rotated_results[3]['thumbnail'].first.last + '.jpg')
+  send_event('preview_thumbnail_4', image: rotated_results[4]['thumbnail'].first.last + '.jpg')
+end
