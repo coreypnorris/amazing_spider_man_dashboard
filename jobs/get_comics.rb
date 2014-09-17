34d84adfb07e5c7ec0345f0c63cb3fa8f076fd4c
Corey Norris <coreypnorris@gmail.com>
2014-09-16 20:55:54 -0700

    create amazing_spider_man dashboard

--- jobs/get_comics.rb
+++ jobs/get_comics.rb
@@ -13,3 +13,21 @@ SCHEDULER.every '1h', :first_in => 0 do |job|
   parsed_data = JSON.parse(returned_data)
   $results = parsed_data['data']['results']
 end
+
+SCHEDULER.every '1h', :first_in => 0 do |job|
+  timestamp = Time.now.to_s.gsub(/\s+/, "")
+  hash = Digest::MD5.hexdigest(timestamp + settings.marvel_private_key + settings.marvel_public_key)
+
+  returned_data = RestClient::Request.new(
+    :method => :get,
+    :url => 'http://gateway.marvel.com:80/v1/public/series/1987/comics?ts=' + timestamp + '&apikey=' + settings.marvel_public_key + '&hash=' + hash + '&formatType=comic&noVariants=true&orderBy=issueNumber'
+  ).execute
+
+  parsed_data = JSON.parse(returned_data)
+  $amazing_spider_man_results = parsed_data['data']['results']
+end
+
+# image               title
+#                     description
+# creators    saleDate price
+
--- jobs/show_comics.rb
+++ jobs/show_comics.rb
@@ -3,11 +3,36 @@ require_relative 'get_comics.rb'
 
 SCHEDULER.every '5s', :first_in => '10s' do |job|
   rotated_results = $results.rotate!
-  send_event('issue_big_picture', image: rotated_results[0]['images'].first['path'] + '.jpg')
-  send_event('issue_title', title: rotated_results[0]['title'])
-  send_event('issue_description', text: rotated_results[0]['description'])
-  send_event('preview_thumbnail_1', image: rotated_results[1]['thumbnail'].first.last + '.jpg')
-  send_event('preview_thumbnail_2', image: rotated_results[2]['thumbnail'].first.last + '.jpg')
-  send_event('preview_thumbnail_3', image: rotated_results[3]['thumbnail'].first.last + '.jpg')
-  send_event('preview_thumbnail_4', image: rotated_results[4]['thumbnail'].first.last + '.jpg')
+  send_event('twc_issue_big_picture', image: rotated_results[0]['images'].first['path'] + '.jpg')
+  send_event('twc_issue_title', title: rotated_results[0]['title'])
+  send_event('twc_issue_description', text: rotated_results[0]['description'])
+  send_event('twc_preview_thumbnail_1', image: rotated_results[1]['thumbnail'].first.last + '.jpg')
+  send_event('twc_preview_thumbnail_2', image: rotated_results[2]['thumbnail'].first.last + '.jpg')
+  send_event('twc_preview_thumbnail_3', image: rotated_results[3]['thumbnail'].first.last + '.jpg')
+  send_event('twc_preview_thumbnail_4', image: rotated_results[4]['thumbnail'].first.last + '.jpg')
+end
+
+SCHEDULER.every '20s', :first_in => '10s' do |job|
+  rotated_results = $amazing_spider_man_results.rotate!
+
+  creators_string = ""
+
+  rotated_results[0]["creators"]["items"].each do |item|
+    creators_string += item["name"] + " " + item["role"]
+    if item == rotated_results[0]["creators"]["items"].last
+      creators_string += "."
+    else
+      creators_string += ", "
+    end
+  end
+
+  sale_date = DateTime.parse(rotated_results[0]["dates"][0]["date"]).to_date.to_s
+
+  send_event('asm_issue_big_picture', image: rotated_results[0]['images'].first['path'] + '.jpg')
+  send_event('asm_issue_title', title: rotated_results[0]['title'])
+  send_event('asm_issue_description', text: rotated_results[0]['description'])
+  send_event('asm_creators', text: creators_string)
+  send_event('asm_sale_date', text: sale_date)
+  # send_event('asm_preview_thumbnail_3', text: rotated_results[0])
+  # send_event('asm_preview_thumbnail_4', text: rotated_results[0])
 end
