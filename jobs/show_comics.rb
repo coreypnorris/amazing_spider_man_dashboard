f7d51a47b5e765c585c74a53881796c79ffa5296
Corey Norris <coreypnorris@gmail.com>
2014-09-21 16:38:07 -0700

    remove dashboards for heroku

--- jobs/show_comics.rb
+++ jobs/show_comics.rb
@@ -15,54 +15,54 @@ SCHEDULER.every '5s', :first_in => '10s' do |job|
   $weekly_comics_results.rotate!
 end
 
-# Amazing Spider-Man dashboard
-SCHEDULER.every '20s', :first_in => '10s' do |job|
-  creators_string = ""
+# # Amazing Spider-Man dashboard
+# SCHEDULER.every '20s', :first_in => '10s' do |job|
+#   creators_string = ""
 
-  $asm_results[0]["creators"]["items"].each do |item|
-    creators_string += item["name"] + " " + item["role"]
-    if item == $asm_results[0]["creators"]["items"].last
-      creators_string += "."
-    else
-      creators_string += ", "
-    end
-  end
+#   $asm_results[0]["creators"]["items"].each do |item|
+#     creators_string += item["name"] + " " + item["role"]
+#     if item == $asm_results[0]["creators"]["items"].last
+#       creators_string += "."
+#     else
+#       creators_string += ", "
+#     end
+#   end
 
-  sale_date = DateTime.parse($asm_results[0]["dates"][0]["date"]).to_date
-  formatted_sale_date = sale_date.strftime("%B %d, %Y")
+#   sale_date = DateTime.parse($asm_results[0]["dates"][0]["date"]).to_date
+#   formatted_sale_date = sale_date.strftime("%B %d, %Y")
 
-  send_event('asm_big_picture', image: $asm_results[0]['images'].first['path'] + '.jpg')
-  send_event('asm_title', text: $asm_results[0]['title'])
-  send_event('asm_description', text: $asm_results[0]["description"].to_s)
-  send_event('asm_creators', text: creators_string)
-  send_event('asm_sale_date', text: formatted_sale_date)
-  send_event('asm_sale_price', text: "$" + $asm_results[0]["prices"][0]["price"].to_s)
+#   send_event('asm_big_picture', image: $asm_results[0]['images'].first['path'] + '.jpg')
+#   send_event('asm_title', text: $asm_results[0]['title'])
+#   send_event('asm_description', text: $asm_results[0]["description"].to_s)
+#   send_event('asm_creators', text: creators_string)
+#   send_event('asm_sale_date', text: formatted_sale_date)
+#   send_event('asm_sale_price', text: "$" + $asm_results[0]["prices"][0]["price"].to_s)
 
-  $asm_results.rotate!
-end
+#   $asm_results.rotate!
+# end
 
-# Captain America dashboard
-SCHEDULER.every '20s', :first_in => '10s' do |job|
-  creators_string = ""
+# # Captain America dashboard
+# SCHEDULER.every '20s', :first_in => '10s' do |job|
+#   creators_string = ""
 
-  $cap_results[0]["creators"]["items"].each do |item|
-    creators_string += item["name"] + " " + item["role"]
-    if item == $cap_results[0]["creators"]["items"].last
-      creators_string += "."
-    else
-      creators_string += ", "
-    end
-  end
+#   $cap_results[0]["creators"]["items"].each do |item|
+#     creators_string += item["name"] + " " + item["role"]
+#     if item == $cap_results[0]["creators"]["items"].last
+#       creators_string += "."
+#     else
+#       creators_string += ", "
+#     end
+#   end
 
-  sale_date = DateTime.parse($cap_results[0]["dates"][0]["date"]).to_date
-  formatted_sale_date = sale_date.strftime("%B %d, %Y")
+#   sale_date = DateTime.parse($cap_results[0]["dates"][0]["date"]).to_date
+#   formatted_sale_date = sale_date.strftime("%B %d, %Y")
 
-  send_event('cap_big_picture', image: $cap_results[0]['images'].first['path'] + '.jpg')
-  send_event('cap_title', text: $cap_results[0]['title'])
-  send_event('cap_description', text: $cap_results[0]["description"].to_s)
-  send_event('cap_creators', text: creators_string)
-  send_event('cap_sale_date', text: formatted_sale_date)
-  send_event('cap_sale_price', text: "$" + $cap_results[0]["prices"][0]["price"].to_s)
+#   send_event('cap_big_picture', image: $cap_results[0]['images'].first['path'] + '.jpg')
+#   send_event('cap_title', text: $cap_results[0]['title'])
+#   send_event('cap_description', text: $cap_results[0]["description"].to_s)
+#   send_event('cap_creators', text: creators_string)
+#   send_event('cap_sale_date', text: formatted_sale_date)
+#   send_event('cap_sale_price', text: "$" + $cap_results[0]["prices"][0]["price"].to_s)
 
-  $cap_results.rotate!
-end
+#   $cap_results.rotate!
+# end
